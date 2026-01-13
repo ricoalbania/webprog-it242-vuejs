087339aa7874378c5cb95b3a39c145e543d29641
Roman Rico Albania <rbalbania2@student.apc.edu.ph>
2026-01-13 02:46:39 +0000

    added component food item

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Mangoes',
+      message: 'I like mangoes'
+    }
+  }
+};
+</script>
+
+<style></style>
