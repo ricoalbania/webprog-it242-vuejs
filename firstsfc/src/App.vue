4cc472fc95f1749b950fd88734e4fb9d8734dff4
Roman Rico Albania <rbalbania2@student.apc.edu.ph>
2026-01-23 10:47:50 +0800

    Task 04

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+const instruments = ref([])
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+onMounted(() => {
+   getInstruments()
+})
+</script>
 <template>
-  <h1>Fruits</h1>
-  <personal-profile/>
-  <food-item/>
-  <food-item2/>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
-
-<script></script>
-
-<style>
-</style>
