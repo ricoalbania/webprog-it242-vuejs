401c456073852f76cd7d53bdf884d2dcb04d9502
Roman Rico Albania <rbalbania2@student.apc.edu.ph>
2026-01-27 10:51:23 +0800

    task 03 add view and insert comment

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -6,6 +6,8 @@ import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 // The "look" - makes the layout work
 import 'bootstrap/dist/css/bootstrap.min.css'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
 // The "brain" - makes the carousel and modals work
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -15,4 +17,6 @@ const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
